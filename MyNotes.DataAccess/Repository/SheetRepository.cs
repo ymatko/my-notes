b904fd712b8f70d7b4575004523f9a96184bb851
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-09-20 15:19:36 +0200

    Add ISheetRepository and SheetRepository

---
+++ MyNotes.DataAccess/Repository/SheetRepository.cs
@@ -0,0 +1,24 @@
+﻿using MyNotes.DataAccess.Data;
+using MyNotes.DataAccess.Repository.IRepository;
+using MyNotes.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes.DataAccess.Repository
+{
+    public class SheetRepository : Repository<Sheet>, ISheetRepository
+    {
+        private ApplicationDbContext _db;
+        public SheetRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Sheet obj)
+        {
+            _db.Sheets.Update(obj);
+        }
+    }
+}
