da6496dc54b1e94688b1177436a427cefaa5487a
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-09-20 15:28:59 +0200

    Add SheetController

---
+++ MyNotes/Controllers/SheetController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyNotes.DataAccess.Repository.IRepository;
+using MyNotes.Models.Models;
+
+namespace MyNotes.Controllers
+{
+    public class SheetController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public SheetController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Sheet> objSheetList = _unitOfWork.Sheet.GetAll().ToList();
+            return View(objSheetList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                //create
+                return View(new Sheet());
+            }
+            else
+            {
+                //update
+                Sheet? sheetFromDb = _unitOfWork.Sheet.Get(u => u.Id == id);
+                return View(sheetFromDb);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Sheet obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (obj.Id == 0)
+                {
+                    _unitOfWork.Sheet.Add(obj);
+                }
+                else
+                {
+                    _unitOfWork.Sheet.Update(obj);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            Sheet? obj = _unitOfWork.Sheet.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Sheet.Remove(obj);
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+    }
+}
