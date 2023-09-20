482aaf38d8527f45dcbd448df536757d67aff5d1
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-09-20 12:24:12 +0200

    Add IUnitOfWork and UnitOfWork

---
+++ MyNotes.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using MyNotes.DataAccess.Data;
+using MyNotes.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
