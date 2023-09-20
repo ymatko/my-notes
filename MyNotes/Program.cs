2ebc6c0496dd00c0c3be396991c23c1ea840e1a8
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-09-20 15:47:38 +0200

    Add UnitOfwork to program.cs

--- MyNotes/Program.cs
+++ MyNotes/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MyNotes.DataAccess.Data;
+using MyNotes.DataAccess.Repository;
+using MyNotes.DataAccess.Repository.IRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
