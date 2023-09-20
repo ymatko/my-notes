99ab58d920360a9167f601bbe84eeff9cc52b580
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-09-20 11:23:05 +0200

    Add sheet model

---
+++ MyNotes.Models/Models/Sheet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes.Models.Models
+{
+    public class Sheet
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(40)]
+        public string? Name { get; set; }
+        public string? Text { get; set; }
+    }
+}
