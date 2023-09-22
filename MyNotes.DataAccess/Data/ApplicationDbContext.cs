b12d734f97258a7479af45f6cd3ab09a60ed796c
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-09-22 19:55:13 +0200

    Add Identity

--- MyNotes.DataAccess/Data/ApplicationDbContext.cs
+++ MyNotes.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MyNotes.Models;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MyNotes.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
