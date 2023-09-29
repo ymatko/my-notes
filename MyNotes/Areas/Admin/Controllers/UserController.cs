21e59c46c583947cb142c35de4f76b234aad5a87
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-09-29 12:43:09 +0200

    Create UserController

---
+++ MyNotes/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyNotes.DataAccess.Repository.IRepository;
+using MyNotes.Utility;
+
+namespace MyNotes.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<IdentityUser> users = _unitOfWork.User.GetAll().ToList();
+            return View(users);
+        }
+    }
+}
