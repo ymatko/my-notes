a411ab8d7199d23b37aab6c716eaceda378401c9
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-09-29 12:42:59 +0200

    Add User to Repository

--- MyNotes.DataAccess/Repository/IRepository/IUserRepository.cs
+++ MyNotes.DataAccess/Repository/IRepository/IUserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,8 @@ using System.Threading.Tasks;
 
 namespace MyNotes.DataAccess.Repository.IRepository
 {
-    public interface IUnitOfWork
+    public interface IUserRepository : IRepository<IdentityUser>
     {
-        ISheetRepository Sheet { get; }
-        IUserRepository User { get; }
-        void Save();
+        public void Update(IdentityUser user);
     }
 }
---
+++ MyNotes.DataAccess/Repository/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MyNotes.DataAccess.Data;
+using MyNotes.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes.DataAccess.Repository
+{
+    public class UserRepository : Repository<IdentityUser>, IUserRepository
+    {
+        private ApplicationDbContext _db;
+        public UserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(IdentityUser user)
+        {
+            _db.Users.Update(user);
+        }
+    }
+}
