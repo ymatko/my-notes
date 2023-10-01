89ce4853b64111d42d820a31dc302de807b50fae
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-10-01 20:25:01 +0200

    Add UserController and Index view

---
+++ MyNotes/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyNotes.DataAccess.Repository.IRepository;
+using MyNotes.Models;
+using MyNotes.Utility;
+
+namespace MyNotes.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = SD.Role_Admin)]
+	public class UserController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public UserController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult Index()
+		{
+			List<ApplicationUser> applicationUsers = _unitOfWork.ApplicationUser.GetAll().ToList();
+			return View(applicationUsers);
+		}
+	}
+}
