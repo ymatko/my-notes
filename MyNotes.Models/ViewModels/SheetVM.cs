67bec275ddab4fd0a93ab9951927ec837879b79f
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-10-13 12:48:59 +0200

    Add SheetVM model

---
+++ MyNotes.Models/ViewModels/SheetVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes.Models.ViewModels
+{
+    public class SheetVM
+    {
+        public List<Sheet> UserSheets { get; set; }
+        public List<Sheet> AdminSheets { get; set; }
+    }
+}
