fb9002b66517cb3a5bfe1dbc5f3630c567d4c1df
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-10-21 20:39:30 +0200

    Add Tab Controller

---
+++ MyNotes/Areas/User/Controllers/TabController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyNotes.DataAccess.Repository.IRepository;
+using MyNotes.Models;
+using System.Security.Claims;
+
+namespace MyNotes.Areas.User.Controllers
+{
+	[Area("User")]
+	[Authorize]
+	public class TabController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public TabController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult Index()
+		{
+			List<Tab> objUserTabList = _unitOfWork.Tab.GetAll().Where(t => t.Sheet.ApplicationUserId == HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value).ToList();
+			return View(objUserTabList);
+		}
+	}
+}
