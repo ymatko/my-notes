c77e54e77af85785ff820e1bba2b557346f474e2
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-10-22 20:07:52 +0200

    Add TabVM

---
+++ MyNotes.Models/ViewModels/TabVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes.Models.ViewModels
+{
+    public class TabVM
+    {
+        public SheetVM Sheets { get; set; }
+        public List<Tab> Tabs { get; set; }
+    }
+}
