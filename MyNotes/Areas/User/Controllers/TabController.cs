99368445c1cad68a59510fbd5131515adb36f64f
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-10-22 20:08:40 +0200

    Add Sheet Index insert Tab Index

--- MyNotes/Areas/User/Controllers/TabController.cs
+++ MyNotes/Areas/User/Controllers/TabController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MyNotes.DataAccess.Repository.IRepository;
 using MyNotes.Models;
+using MyNotes.Models.ViewModels;
 using System.Security.Claims;
 
 namespace MyNotes.Areas.User.Controllers
@@ -18,7 +19,22 @@ namespace MyNotes.Areas.User.Controllers
 		public IActionResult Index()
 		{
 			List<Tab> objUserTabList = _unitOfWork.Tab.GetAll().Where(t => t.Sheet.ApplicationUserId == HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value).ToList();
-			return View(objUserTabList);
+            List<Sheet> objUserSheetList = _unitOfWork.Sheet.GetAll()
+               .Where(s => s.ApplicationUserId == HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value && s.InTrash == false).ToList();
+            List<Sheet> objAdminSheetList = _unitOfWork.Sheet.GetAll()
+                .Where(s => s.ApplicationUserId == "66b75e85-857a-47b6-9fd7-1312a9d88671" && s.InTrash == false).ToList();
+
+			TabVM compositeModel = new TabVM()
+			{
+				Sheets = new SheetVM()
+				{
+                    UserSheets = objUserSheetList,
+                    AdminSheets = objAdminSheetList
+                },
+				Tabs = objUserTabList
+			};
+			
+			return View(compositeModel);
 		}
 	}
 }
